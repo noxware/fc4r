a5b96957d29bc8ed3355f86973fb250ffc916bfa
Noxware <7684329+noxware@users.noreply.github.com>
2023-06-09 00:17:17 -0300

    new settings toml support for general config

--- src/core/config.rs
+++ src/core/config.rs
@@ -1,22 +1,32 @@
+use serde::Deserialize;
 use std::error::Error;
 use std::fs;
-use std::path::PathBuf;
+use std::path::Path;
 
 use super::label::LabelLibrary;
 
 pub struct Config {
     pub labels: LabelLibrary,
+    pub settings: Settings,
+}
+
+#[derive(Deserialize)]
+pub struct Settings {
+    pub link_dir: String,
 }
 
 impl Config {
     // TODO: Remove file system dependency from core.
     pub fn load(dir_path: &str) -> Result<Self, Box<dyn Error>> {
-        let mut labels_path = PathBuf::from(dir_path);
-        labels_path.push("labels.toml");
-
+        let labels_path = Path::new(dir_path).join("labels.toml");
         let labels_content = fs::read_to_string(labels_path)?;
         let labels = LabelLibrary::from_toml(&labels_content)?;
-        let config = Config { labels };
+
+        let settings_path = Path::new(dir_path).join("settings.toml");
+        let settings_content = fs::read_to_string(settings_path)?;
+        let settings: Settings = toml::from_str(&settings_content)?;
+
+        let config = Config { labels, settings };
 
         Ok(config)
     }
@@ -30,11 +40,14 @@ mod tests {
     fn load_works() {
         let config = Config::load("test_dir/fileclass").unwrap();
         let labels = config.labels;
+        let settings = config.settings;
 
         let label_name = labels.resolve("alias");
         let label_description = labels.get_description("label");
-
         assert_eq!(label_name, "label");
         assert_eq!(label_description, "a label");
+
+        let link_dir = settings.link_dir;
+        assert_eq!(link_dir, "test_dir/fileclass/temp/links");
     }
 }
