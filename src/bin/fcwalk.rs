49fd9a214aaae0e9799511e5072f4c9e95dd99ab
Noxware <7684329+noxware@users.noreply.github.com>
2023-06-09 13:06:52 -0300

    fcwalk - avoid traversing fileclass dir

--- src/bin/fcwalk.rs
+++ src/bin/fcwalk.rs
@@ -1,10 +1,9 @@
-// ChatGPT generated code.
-
-// TODO: Remake.
 // TODO: Add a way to ignore certain directories. Specially the `fileclass` dir.
 
 use std::env;
 use std::fs;
+
+use fileclass::core::config::STD_CONFIG_DIR;
 
 fn main() {
     // Get the current directory
@@ -20,14 +19,18 @@ fn traverse_directory(path: &std::path::Path, base_path: &std::path::Path) {
         for entry in entries {
             if let Ok(entry) = entry {
                 let entry_path = entry.path();
-                let relative_path = entry_path.strip_prefix(base_path).unwrap();
 
-                // Print the relative entry path
-                println!("{}", relative_path.display());
+                // Omit the fileclass directory.
+                if !entry_path.ends_with(STD_CONFIG_DIR) {
+                    let relative_path = entry_path.strip_prefix(base_path).unwrap();
 
-                // Recursively traverse subdirectories
-                if entry_path.is_dir() {
-                    traverse_directory(&entry_path, base_path);
+                    // Print the relative entry path
+                    println!("{}", relative_path.display());
+
+                    // Recursively traverse subdirectories
+                    if entry_path.is_dir() {
+                        traverse_directory(&entry_path, base_path);
+                    }
                 }
             }
         }
